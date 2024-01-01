1500c7158c84bb47bdada2b827f46d912ea557fc
brutalX <143045891+brutalX-04@users.noreply.github.com>
2024-01-01 22:45:31 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,109 +1,45 @@
-import random
-import requests
-from concurrent.futures import ThreadPoolExecutor as tp
-
-rd = random.randrange
+import requests, random
+from concurrent.futures import ThreadPoolExecutor
 
 
-def step1(nomer):
-	headers = {
-	    'authority': 'd4n4programterbaru.info-danna.bio',
-	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	    'accept-language': 'en-US,en;q=0.9,id;q=0.8',
-	    'cache-control': 'max-age=0',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://d4n4programterbaru.info-danna.bio',
-	    'referer': 'https://d4n4programterbaru.info-danna.bio/main.php',
-	    'sec-fetch-dest': 'document',
-	    'sec-fetch-mode': 'navigate',
-	    'sec-fetch-site': 'same-origin',
-	    'sec-fetch-user': '?1',
-	    'upgrade-insecure-requests': '1',
-	    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-	}
 
-	data = {
-	    'step': '1',
-	    'phone': nomer,
-	}
+def mulai(nope,name):
+	try:
+		headers = {
+		    'authority': 'licensi.yayanxd.my.id',
+		    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+		    'accept-language': 'en-US,en;q=0.9,id;q=0.8',
+		    'cache-control': 'no-cache',
+		    'content-type': 'application/x-www-form-urlencoded',
+		    'origin': 'https://licensi.yayanxd.my.id',
+		    'pragma': 'no-cache',
+		    'referer': 'https://licensi.yayanxd.my.id/login.php',
+		    'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+		    'sec-ch-ua-mobile': '?0',
+		    'sec-ch-ua-platform': '"Linux"',
+		    'sec-fetch-dest': 'document',
+		    'sec-fetch-mode': 'navigate',
+		    'sec-fetch-site': 'same-origin',
+		    'sec-fetch-user': '?1',
+		    'upgrade-insecure-requests': '1',
+		    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+		}
 
-	post = requests.post('https://d4n4programterbaru.info-danna.bio/send_date.php', headers=headers, data=data)
-	if '<Response [200]>' in str(post):
-		print('Succes Step1')
-	else:
-		print('Failled Step1')
+		data = {
+		    'username': 'babasid',
+		    'password': 'babsidx',
+		    'submit': 'ok',
+		}
 
+		response = requests.post('https://licensi.yayanxd.my.id/login.php', headers=headers, data=data)
+		print('Status : %s, %s|%s'%(response.status_code,nope,name))
 
-def step2():
-	headers = {
-	    'authority': 'd4n4programterbaru.info-danna.bio',
-	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	    'accept-language': 'en-US,en;q=0.9,id;q=0.8',
-	    'cache-control': 'max-age=0',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://d4n4programterbaru.info-danna.bio',
-	    'referer': 'https://d4n4programterbaru.info-danna.bio/masuk.php',
-	    'sec-fetch-dest': 'document',
-	    'sec-fetch-mode': 'navigate',
-	    'sec-fetch-site': 'same-origin',
-	    'sec-fetch-user': '?1',
-	    'upgrade-insecure-requests': '1',
-	    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-	}
-
-	data = {
-	    'step': '2',
-	    'pin1': rd(0,9),
-	    'pin2': rd(0,9),
-	    'pin3': rd(0,9),
-	    'pin4': rd(0,9),
-	    'pin5': rd(0,9),
-	    'pin6': rd(0,9),
-	}
-
-	post = requests.post('https://d4n4programterbaru.info-danna.bio/send_date.php', headers=headers, data=data)
-	if '<Response [200]>' in str(post):
-		print('Succes Step2')
-	else:
-		print('Failled Step2')
-
-def step3():
-	headers = {
-	    'authority': 'd4n4programterbaru.info-danna.bio',
-	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-	    'accept-language': 'en-US,en;q=0.9,id;q=0.8',
-	    'cache-control': 'max-age=0',
-	    'content-type': 'application/x-www-form-urlencoded',
-	    'origin': 'https://d4n4programterbaru.info-danna.bio',
-	    'referer': 'https://d4n4programterbaru.info-danna.bio/verifsms.php',
-	    'sec-fetch-dest': 'document',
-	    'sec-fetch-mode': 'navigate',
-	    'sec-fetch-site': 'same-origin',
-	    'sec-fetch-user': '?1',
-	    'upgrade-insecure-requests': '1',
-	    'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-	}
-
-	data = {
-	    'step': '3',
-	    'otp1': rd(0,9),
-	    'otp2': rd(0,9),
-	    'otp3': rd(0,9),
-	    'otp4': rd(0,9)
-	}
-
-	post = requests.post('https://d4n4programterbaru.info-danna.bio/send_date.php', headers=headers, data=data)
-	if '<Response [200]>' in str(post):
-		print('Succes Step3')
-	else:
-		print('Failled Step3')
-	
+	except Exception as e:
+		print(e)
 
 
-file = open('bahan.txt', 'r').readlines()
-for data in file:
-	nomer = data.replace('\n','')
-	with tp(max_workers=30) as gas:
-		gas.submit(step1, nomer)
-		gas.submit(step2)
-		gas.submit(step3)
+for x in open('account.txt', 'r').readlines():
+	nope,name = x.split('|')[0],x.split('|')[1].replace('\n','')
+	with ThreadPoolExecutor(max_workers=100) as gasken:
+		for i in range(100):
+			gasken.submit(mulai, nope, name)
